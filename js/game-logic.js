3fd56198bf06540cfddaa789f7699c73940cac77
A-I-18 <antonio.ionta.18@gmail.com>
2025-07-11 12:40:19 +0200

    feat: function for setting player moves
    edge cases

--- js/game-logic.js
+++ js/game-logic.js
@@ -4,7 +4,47 @@ let playerTwoMoveOneType, playerTwoMoveTwoType, playerTwoMoveThreeType;
 let playerOneMoveOneValue, playerOneMoveTwoValue, playerOneMoveThreeValue;
 let playerTwoMoveOneValue, playerTwoMoveTwoValue, playerTwoMoveThreeValue;
 
+function isValidMoveType(moveType) {
+    // Move type missing
+    if (typeof moveType !== 'string') {
+        return false;
+    }
+
+    // Move type invalid
+    if (moveType !== 'rock' && moveType !== 'paper' && moveType !== 'scissors') {
+        return false;
+    }
+    
+    return true;
+}
+
+function isValidMoveValue(moveValue) {
+    // Move value missing
+    if (typeof moveValue !== 'number') {
+        return false;
+    }
+
+    // Move value invalid
+    if (moveValue < 1 || moveValue > 99) {
+        return false;
+    }
+
+    return true;
+}
+
 function setPlayerMoves(player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) {
+    // Edge cases
+    let areValidMoveTypes = isValidMoveType(moveOneType) && isValidMoveType(moveTwoType) && isValidMoveType(moveThreeType);
+    let areValidMoveValues = isValidMoveValue(moveOneValue) && isValidMoveValue(moveTwoValue) && isValidMoveValue(moveThreeValue);
+
+    if (!areValidMoveTypes || !areValidMoveValues) {
+        return;
+    }
+
+    if (moveOneValue + moveTwoValue + moveThreeValue > 99) {
+        return;
+    }
+    
     if (player === 'Player One') {
         playerOneMoveOneType = moveOneType;
         playerOneMoveTwoType = moveTwoType;
