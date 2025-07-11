e15bba5746bcbf2984b1f9d06048a6e131216044
A-I-18 <antonio.ionta.18@gmail.com>
2025-07-11 10:47:04 +0200

    feat: function for setting player moves
    main functionality

--- js/game-logic.js
+++ js/game-logic.js
@@ -3,3 +3,23 @@ let playerTwoMoveOneType, playerTwoMoveTwoType, playerTwoMoveThreeType;
 
 let playerOneMoveOneValue, playerOneMoveTwoValue, playerOneMoveThreeValue;
 let playerTwoMoveOneValue, playerTwoMoveTwoValue, playerTwoMoveThreeValue;
+
+function setPlayerMoves(player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) {
+    if (player === 'Player One') {
+        playerOneMoveOneType = moveOneType;
+        playerOneMoveTwoType = moveTwoType;
+        playerOneMoveThreeType = moveThreeType;
+
+        playerOneMoveOneValue = moveOneValue;
+        playerOneMoveTwoValue = moveTwoValue;
+        playerOneMoveThreeValue = moveThreeValue;
+    } else if (player === 'Player Two') {
+        playerTwoMoveOneType = moveOneType;
+        playerTwoMoveTwoType = moveTwoType;
+        playerTwoMoveThreeType = moveThreeType;
+
+        playerTwoMoveOneValue = moveOneValue;
+        playerTwoMoveTwoValue = moveTwoValue;
+        playerTwoMoveThreeValue = moveThreeValue;
+    }
+}
